5fb1373aa0c2191ace49953848ee4340ce405b1e
Jose Ivan Ribeiro de Oliveira <ivan_r.o@hotmail.com>
2021-01-06 21:02:22 -0300

    dev: project initialized with web spring initializer.

---
+++ backend/src/main/java/com/estrngdeliver/estrngdeliver/EstrngdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.estrngdeliver.estrngdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstrngdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstrngdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/estrngdeliver/estrngdeliver/EstrngdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estrngdeliver.estrngdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstrngdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
