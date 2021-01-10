d093dd663949ef045b331430bb9598adb390026c
Jose Ivan Ribeiro de Oliveira <ivan_r.o@hotmail.com>
2021-01-09 22:27:01 -0300

    dev: Home page created.

---
+++ webapp/src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import "./styles.css";
+import { ReactComponent as MainImage } from "./main.svg";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <div className="home-actiions">
+          <h1 className="home-title">Faça seu pedido!</h1>
+          <h3 className="home-sobtitle">
+            Faça seu pedido escolha os itens <br /> que lhe entregaremos
+          </h3>
+          <a href="/order" rel="noopener noreferrer">
+            Fazer pedido
+          </a>
+        </div>
+        <div className="home-image">
+          <MainImage />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
