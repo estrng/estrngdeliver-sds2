55457a03db652a2fc1b43e7e1af75833ccdcfa42
Jose Ivan Ribeiro de Oliveira <ivan_r.o@hotmail.com>
2021-01-10 16:53:52 -0300

    ajustment: cleanup unused vars.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Routes from "./Routes";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 
